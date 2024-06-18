6915c55cdc0714b10c6811f9718353fa2b5f55b4
quelcev <quelcev@gmail.com>
2024-06-19 03:26:01 +0800

    Dad Jokes project complete

---
+++ dad-jokes/main.js
@@ -0,0 +1,19 @@
+const generateBtnEl = document.querySelector(".generate-btn");
+const resultEl = document.querySelector(".result");
+
+fetchJoke();
+generateBtnEl.addEventListener("click", fetchJoke);
+
+async function fetchJoke() {
+  try {
+    const response = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "text/plain",
+      },
+    });
+    const jokeText = await response.text();
+    resultEl.textContent = jokeText;
+  } catch (error) {
+    console.log(error);
+  }
+}
